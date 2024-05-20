80300038c484c35f3ca61cdf0d7d59d8fcc16037
Cristian <cristian.beler@student.upt.ro>
2024-05-20 20:56:07 +0300

    Actualizare functionalitate HotelSearch

---
+++ HotelSearchForm.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Hotel_Management_System
+{
+    partial class HotelSearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HotelSearchForm));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button_search = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_hotelName = new System.Windows.Forms.TextBox();
+            this.label_exit = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dataGridView_hotelSearch = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_hotelSearch)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button_search);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 747);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1475, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(730, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(730, 100);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Clean";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_search
+            // 
+            this.button_search.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button_search.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button_search.FlatAppearance.BorderSize = 0;
+            this.button_search.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_search.ForeColor = System.Drawing.Color.Red;
+            this.button_search.Image = ((System.Drawing.Image)(resources.GetObject("button_search.Image")));
+            this.button_search.Location = new System.Drawing.Point(0, 0);
+            this.button_search.Name = "button_search";
+            this.button_search.Size = new System.Drawing.Size(730, 100);
+            this.button_search.TabIndex = 0;
+            this.button_search.Text = "Search";
+            this.button_search.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button_search.UseVisualStyleBackColor = true;
+            this.button_search.Click += new System.EventHandler(this.button_search_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(575, 594);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Hotel Search:";
+            // 
+            // textBox_hotelName
+            // 
+            this.textBox_hotelName.Location = new System.Drawing.Point(726, 591);
+            this.textBox_hotelName.Name = "textBox_hotelName";
+            this.textBox_hotelName.Size = new System.Drawing.Size(173, 32);
+            this.textBox_hotelName.TabIndex = 3;
+            // 
+            // label_exit
+            // 
+            this.label_exit.AutoSize = true;
+            this.label_exit.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_exit.ForeColor = System.Drawing.Color.Red;
+            this.label_exit.Location = new System.Drawing.Point(1427, 0);
+            this.label_exit.Name = "label_exit";
+            this.label_exit.Size = new System.Drawing.Size(29, 29);
+            this.label_exit.TabIndex = 7;
+            this.label_exit.Text = "X";
+            this.label_exit.Click += new System.EventHandler(this.label_exit_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(655, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(159, 29);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Hotel Search";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label_exit);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1475, 100);
+            this.panel2.TabIndex = 8;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.dataGridView_hotelSearch);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 100);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1475, 443);
+            this.panel3.TabIndex = 9;
+            // 
+            // dataGridView_hotelSearch
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.dataGridView_hotelSearch.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView_hotelSearch.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView_hotelSearch.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView_hotelSearch.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView_hotelSearch.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dataGridView_hotelSearch.Location = new System.Drawing.Point(12, 6);
+            this.dataGridView_hotelSearch.Name = "dataGridView_hotelSearch";
+            this.dataGridView_hotelSearch.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dataGridView_hotelSearch.RowHeadersVisible = false;
+            this.dataGridView_hotelSearch.RowHeadersWidth = 72;
+            this.dataGridView_hotelSearch.Size = new System.Drawing.Size(1471, 434);
+            this.dataGridView_hotelSearch.TabIndex = 2;
+            this.dataGridView_hotelSearch.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.dataGridView_hotelSearch.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.dataGridView_hotelSearch.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.dataGridView_hotelSearch.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.dataGridView_hotelSearch.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.dataGridView_hotelSearch.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.dataGridView_hotelSearch.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dataGridView_hotelSearch.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.dataGridView_hotelSearch.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView_hotelSearch.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView_hotelSearch.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.dataGridView_hotelSearch.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.dataGridView_hotelSearch.ThemeStyle.HeaderStyle.Height = 23;
+            this.dataGridView_hotelSearch.ThemeStyle.ReadOnly = false;
+            this.dataGridView_hotelSearch.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.dataGridView_hotelSearch.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dataGridView_hotelSearch.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView_hotelSearch.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.Red;
+            this.dataGridView_hotelSearch.ThemeStyle.RowsStyle.Height = 22;
+            this.dataGridView_hotelSearch.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dataGridView_hotelSearch.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.dataGridView_hotelSearch.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_hotelSearch_CellContentClick_1);
+            // 
+            // HotelSearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1475, 847);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_hotelName);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "HotelSearchForm";
+            this.Text = "HotelSearchForm";
+            this.Load += new System.EventHandler(this.HotelSearchForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_hotelSearch)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_search;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_hotelName;
+        private System.Windows.Forms.Label label_exit;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private Guna.UI2.WinForms.Guna2DataGridView dataGridView_hotelSearch;
+    }
+}
--- MainFormClient.cs
+++ MainFormClient.cs
@@ -17,11 +17,8 @@ namespace Hotel_Management_System
             InitializeComponent();
         }
 
-        private void label2_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
 
+<<<<<<< Updated upstream
         private void button_logout_Click(object sender, EventArgs e)
         {
             panel_slide.Height = button_logout.Height;
@@ -32,17 +29,35 @@ namespace Hotel_Management_System
         }
 
         private void button_hotelSearch_Click(object sender, EventArgs e)
+=======
+        private void button_hotelSearch_Click_1(object sender, EventArgs e)
+>>>>>>> Stashed changes
         {
             panel_slide.Height = button_hotelSearch.Height;
             panel_slide.Top = button_hotelSearch.Top;
 
             panel_main.Controls.Clear();
-            HotelSearch hotelSearch = new HotelSearch();
+            HotelSearchForm hotelSearch = new HotelSearchForm();
             hotelSearch.TopLevel = false;
             hotelSearch.Dock = DockStyle.Fill;
             hotelSearch.FormBorderStyle = FormBorderStyle.None;
             panel_main.Controls.Add(hotelSearch);
             hotelSearch.Show();
         }
+<<<<<<< Updated upstream
+=======
+
+        private void button_logout_Click_1(object sender, EventArgs e)
+        {
+            LoginForm login = new LoginForm();
+            this.Hide();
+            login.Show();
+        }
+
+        private void label2_Click_2(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+>>>>>>> Stashed changes
     }
 }
