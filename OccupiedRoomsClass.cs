2f60df6879ea6d23bfffbe0849e1f66541d09ed4
Cristian <cristian.beler@student.upt.ro>
2024-05-22 03:38:52 +0300

    Implementare functionalitati pentru a vedea camerele ocupate

---
+++ OccupiedRoomsClass.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Hotel_Management_System
+{
+    internal class OccupiedRoomsClass
+    {
+        DataBaseConnection connection = new DataBaseConnection();
+        public bool selectRooms(string id, int floor, string phone, string status, string type)
+        {
+            string insertQuery = "select r.roomID,r.roomType,r.roomPhone,r.roomStatus,r.roomFloor from room r join reservation res on r.roomID = res.roomID where r.roomStatus = 'Busy' AND extract(day from CURRENT_DATE) BETWEEN extract(day from res.dateIn) and extract(day from res.dateOut) ";
+            MySqlCommand command = new MySqlCommand(insertQuery, connection.GetConnection());
+            command.Parameters.Add("@id", MySqlDbType.VarChar).Value = id;
+            command.Parameters.Add("@floor", MySqlDbType.Int32).Value = floor;
+            command.Parameters.Add("@phone", MySqlDbType.VarChar).Value = phone;
+            command.Parameters.Add("@status", MySqlDbType.VarChar).Value = status;
+            command.Parameters.Add("@type", MySqlDbType.VarChar).Value = type;
+
+            connection.OpenCon();
+            if (command.ExecuteNonQuery() == 1)
+            {
+                connection.CloseCon();
+                return true;
+            }
+            else
+            {
+                connection.CloseCon();
+                return false;
+            }
+        }
+    }
+}
---
+++ OccupiedRoomsForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Hotel_Management_System
+{
+    partial class OccupiedRoomsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OccupiedRoomsForm));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button_clean = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 15.85714F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1436, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 27.85714F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(599, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(325, 43);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Occupied Rooms";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(4, 745);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(748, 100);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Search";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button_clean
+            // 
+            this.button_clean.FlatAppearance.BorderSize = 0;
+            this.button_clean.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_clean.ForeColor = System.Drawing.Color.Red;
+            this.button_clean.Image = ((System.Drawing.Image)(resources.GetObject("button_clean.Image")));
+            this.button_clean.Location = new System.Drawing.Point(748, 745);
+            this.button_clean.Name = "button_clean";
+            this.button_clean.Size = new System.Drawing.Size(725, 100);
+            this.button_clean.TabIndex = 7;
+            this.button_clean.Text = "Clean";
+            this.button_clean.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button_clean.UseVisualStyleBackColor = true;
+            // 
+            // OccupiedRoomsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
+            this.ClientSize = new System.Drawing.Size(1475, 847);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_clean);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Red;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "OccupiedRoomsForm";
+            this.Text = "OccupiedRoomsForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_clean;
+    }
+}
