bd913545ed8cd4992ee4eab829ce4c0a9a138818
Cristian <cristian.beler@student.upt.ro>
2024-05-27 10:44:40 +0300

    Adaugare teste automate pentru DenyRoomsClass

--- Tests/DenyRoomsTests.cs
+++ Tests/DenyRoomsTests.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using System;
+using Hotel_Management_System;
+using System.Data;
 
-namespace Tests
+namespace HotelManagementSystem.Tests
 {
-    internal class DenyRoomsTests
+    [TestFixture]
+    public class DenyRoomsClassTests
     {
+        private DenyRoomsClass denyRooms;
+
+        [SetUp]
+        public void Setup()
+        {
+            denyRooms = new DenyRoomsClass();
+        }
+
+        [Test]
+        public void DeleteReservation_ExistingReservationID_ReturnsTrue()
+        {
+            int existingReservationID = 1;
+            bool result = denyRooms.deleteReservation(existingReservationID);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void DeleteReservation_NonExistingReservationID_ReturnsFalse()
+        {
+            int nonExistingReservationID = 9999;
+            bool result = denyRooms.deleteReservation(nonExistingReservationID);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void GetReservation_ReturnsDataTable()
+        {
+            DataTable result = denyRooms.getReservation();
+
+            Assert.IsNotNull(result);
+            Assert.Greater(result.Rows.Count, 0);
+        }
     }
 }
