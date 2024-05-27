bd66dc03503fec1475f397ea171e9a6c4f714f80
Cristian <cristian.beler@student.upt.ro>
2024-05-27 10:46:20 +0300

    Adaugare teste automate pentru HotelSearch

--- Tests/HotelSearchTests.cs
+++ Tests/HotelSearchTests.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using System;
+using Hotel_Management_System;
 
-namespace Tests
+namespace HotelManagementSystem.Tests
 {
-    internal class HotelSearchTests
+    [TestFixture]
+    public class HotelSearchClassTests
     {
+        private HotelSearchClass hotelSearch;
+
+        [SetUp]
+        public void Setup()
+        {
+            hotelSearch = new HotelSearchClass();
+        }
+
+        [Test]
+        public void SelectHotel_ExistingHotelName_ReturnsTrue()
+        {
+            string existingHotelName = "Hotel A";
+            bool result = hotelSearch.selectHotel(existingHotelName);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void SelectHotel_NonExistingHotelName_ReturnsFalse()
+        {
+            string nonExistingHotelName = "NonExistingHotel";
+            bool result = hotelSearch.selectHotel(nonExistingHotelName);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void GetHotelName_ExistingHotelName_ReturnsSameName()
+        {
+            string existingHotelName = "Hotel A";
+            string result = hotelSearch.getHotelName(existingHotelName);
+            Assert.AreEqual(existingHotelName, result);
+        }
+
+        [Test]
+        public void GetHotelName_NonExistingHotelName_ReturnsEmptyString()
+        {
+            string nonExistingHotelName = "NonExistingHotel";
+            string result = hotelSearch.getHotelName(nonExistingHotelName);
+            Assert.AreEqual("", result);
+        }
     }
 }
